dcbe19e05faa17c2dad416832e0db760b48ede3b
Boban Acimovic <boban.acimovic@gmail.com>
2018-09-04 19:39:57 +0200

    fix flight, add video streaming

--- go.mod
+++ go.mod
@@ -2,5 +2,6 @@ module github.com/acim/tello
 
 require (
 	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/pkg/errors v0.8.0
 	gobot.io/x/gobot v1.12.0
 )
--- test.go
+++ test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os/exec"
 	"time"
 
+	"github.com/pkg/errors"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
 )
@@ -16,24 +17,23 @@ func main() {
 		mplayer := exec.Command("mplayer", "-fps", "25", "-")
 		mplayerIn, _ := mplayer.StdinPipe()
 		if err := mplayer.Start(); err != nil {
-			fmt.Println(err)
-			fmt.Println(err)
+			log.Print(errors.Wrap(err, "mplayer.Start"))
 		}
 
 		drone.On(tello.ConnectedEvent, func(data interface{}) {
-			fmt.Println("Connected")
+			log.Print("Connected")
 			err := drone.StartVideo()
 			if err != nil {
-				fmt.Println(err)
+				log.Print(errors.Wrap(err, "drone.StartVideo"))
 			}
 			err = drone.SetVideoEncoderRate(4)
 			if err != nil {
-				fmt.Println(err)
+				log.Print(errors.Wrap(err, "drone.SetVideoEncoderRate"))
 			}
 			gobot.Every(100*time.Millisecond, func() {
 				err := drone.StartVideo()
 				if err != nil {
-					fmt.Println(err)
+					log.Print(errors.Wrap(err, "drone.StartVideo"))
 				}
 			})
 		})
@@ -41,35 +41,41 @@ func main() {
 		drone.On(tello.VideoFrameEvent, func(data interface{}) {
 			pkt := data.([]byte)
 			if _, err := mplayerIn.Write(pkt); err != nil {
-				fmt.Println(err)
+				log.Print(errors.Wrap(err, "mplayerIn.Write"))
 			}
 		})
 
-		// err := drone.TakeOff()
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
+		drone.On(tello.FlightDataEvent, func(data interface{}) {
+			fd := data.(*tello.FlightData)
+			log.Printf("Height: %d Battery: %d Remaining Flytime: %d\n", fd.Height, fd.BatteryPercentage, fd.DroneFlyTimeLeft)
+		})
 
-		// gobot.After(100*time.Millisecond, func() {
-		// 	err := drone.Forward(1)
-		// 	if err != nil {
-		// 		fmt.Println(err)
-		// 	}
-		// })
+		log.Print("droneTakeOff")
+		err := drone.TakeOff()
+		if err != nil {
+			log.Print(errors.Wrap(err, "droneTakeOff"))
+		}
 
-		// gobot.After(100*time.Millisecond, func() {
-		// 	err := drone.Backward(1)
-		// 	if err != nil {
-		// 		fmt.Println(err)
-		// 	}
-		// })
+		gobot.After(2*time.Second, func() {
+			err := drone.Forward(2)
+			if err != nil {
+				log.Print(errors.Wrap(err, "drone.Forward"))
+			}
+		})
 
-		// gobot.After(1*time.Second, func() {
-		// 	err := drone.Land()
-		// 	if err != nil {
-		// 		fmt.Println(err)
-		// 	}
-		// })
+		gobot.After(time.Second, func() {
+			err := drone.Backward(3)
+			if err != nil {
+				log.Print(errors.Wrap(err, "drone.Backward"))
+			}
+		})
+
+		gobot.After(5*time.Second, func() {
+			err := drone.Land()
+			if err != nil {
+				log.Print(errors.Wrap(err, "mplayer.Start"))
+			}
+		})
 	}
 
 	robot := gobot.NewRobot("tello",
@@ -80,6 +86,6 @@ func main() {
 
 	err := robot.Start()
 	if err != nil {
-		fmt.Println(err)
+		log.Print(errors.Wrap(err, "robot.Start"))
 	}
 }
